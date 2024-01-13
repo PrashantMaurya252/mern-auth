2fea5af9b86403af52c7f95ea11d710d14ecc6b3
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-01-13 12:06:59 +0530

    created signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import userRoute from './routes/userRouter.js';
+import authRoute from './routes/authRouter.js';
 dotenv.config();
 
 
@@ -10,6 +12,11 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log(err)
 })
 const app=express();
-app .listen(3000,()=>{
-    console.log('app is listening on 3000')
+app.use(express.json());
+app .listen(5000,()=>{
+    console.log('app is listening on 5000')
 })
+
+app.use('/api/user',userRoute);
+app.use('/api/auth',authRoute);
+
